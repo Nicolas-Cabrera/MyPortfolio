6ad15b979049599f5dd6cdf8e14b2e38e9fbb589
unknown <nicolascc1@hotmail.com>
2019-11-11 17:25:40 +0000

    Created all folders with corresponding JS and CSS files

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -4,22 +4,21 @@ import MobileToggle from '../MobileToggle/MobileToggle.js'
 
 const Sidebar = props => (
     <header className='sidebar'>
-            <nav className='sidebar-nav'>
-                <div className='sidebar-Toggle-Button'>
-                    <MobileToggle click={props.drawerClickHandler} />
-                </div>
-                <div className='sidebar-items'>
-                <div className='logo'><a href=''>Nicolas Cabrera</a></div>
-                    <ul>
-                        <li><a href='/'>Home</a></li>
-                        <li><a href='/'>About</a></li>
-                        <li><a href='/'>Skills</a></li>
-                        <li><a href='/'>Projects</a></li>
-                        <li><a href='/'>Contact</a></li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
+        <nav className='sidebar-nav'>
+            <div className='sidebar-Toggle-Button'>
+                <MobileToggle click={props.drawerClickHandler} />
+            </div>
+            <div className='sidebar-items'>
+                <ul>
+                    <li><a href='/'>Home</a></li>
+                    <li><a href='/'>About</a></li>
+                    <li><a href='/'>Skills</a></li>
+                    <li><a href='/'>Projects</a></li>
+                    <li><a href='/'>Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
 );
 
 export default Sidebar;
