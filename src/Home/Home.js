31710d8d4b1055785ebf82ad402a6481165af1c0
unknown <nicolascc1@hotmail.com>
2019-11-13 16:21:36 +0000

    Typescript added to home page

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import './Home.css'
+import Typewriter from 'typewriter-effect';
 
 const Home = () => {
     return (
-            <div className='content' id='home'>
-                <h1>Nicolas Cabrera</h1>
-                <h2>London based Full-Stack Developer</h2>
-            </div>
+        <div className='content' id='home'>
+            <h1>Nicolas Cabrera</h1>
+            <h1>
+                <Typewriter
+                    onInit={(typewriter) => {
+                        typewriter.typeString('London based Full-Stack Developer!')
+                            .callFunction(() => {
+                                console.log('String typed out!');
+                            })
+                            .pauseFor(2000)
+                            .callFunction(() => {
+                                console.log('All strings were deleted');
+                            })
+                            .start();
+                    }}
+                />
+            </h1>
+        </div>
     );
 }
 export default Home;
