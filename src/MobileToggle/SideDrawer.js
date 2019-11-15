596c7c4a0879ba0c5036ef25dedfc212831f8a01
unknown <nicolascc1@hotmail.com>
2019-11-15 11:41:15 +0000

    Menu supposively fixed

--- src/MobileToggle/SideDrawer.js
+++ src/MobileToggle/SideDrawer.js
@@ -9,11 +9,11 @@ const SideDrawer = props => {
     return (
         <nav className={drawerClasses}>
             <ul>
-                <li><a href='/'>Home</a></li>
-                <li><a href='/'>About</a></li>
-                <li><a href='/'>Skills</a></li>
-                <li><a href='/'>Projects</a></li>
-                <li><a href='/'>Contact</a></li>
+                <li><a href='/#home'>Home</a></li>
+                <li><a href='/#about'>About</a></li>
+                <li><a href='/#skills'>Skills</a></li>
+                <li><a href='/#projects'>Projects</a></li>
+                <li><a href='/#contact'>Contact</a></li>
             </ul>
         </nav>
     );
