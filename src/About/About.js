ac061d3a38d35f5f47156c6e9a13c127675b873a
unknown <nicolascc1@hotmail.com>
2019-12-13 14:40:46 +0000

    Made squares for projects

--- src/About/About.js
+++ src/About/About.js
@@ -6,6 +6,9 @@ const About = () => {
         <>
             <div className='section' id='about'>
                 <h1 className='title'>About Me</h1>
+                <p>An eager, talented and committed graduate developer looking to acquire a position in junior 
+                    software development where I can utilise and improve my programming skills as well as market knowledge.
+                </p>
             </div>
         </>
     )
--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import './Projects.css';
+import kodflix from '../images/kodflix.JPG';
+import footapp from '../images/footapp.JPG';
 
 const Projects = () => {
     return (
         <>
             <div className='section' id='projects'>
                 <h1>Projects</h1>
+                <div className='project-sec'>
+                    <div className='proj-item'>
+                        {/* <img classname='image' src={kodflix} alt='Kodflix'/> */}
+                        <h2>Kodflix</h2>
+                    </div>
+                    <div className='proj-item'>
+                        {/* <img classname='image' src={footapp} alt='Footapp'/> */}
+                        <h2>Footapp</h2>
+                    </div>
+                    <div className='proj-item'>
+                        <h2>Calculator</h2>
+                    </div>
+                </div>
             </div>
         </>
     )
