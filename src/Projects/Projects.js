119fcb2ef118dba0436652f5eb4e23cab02ac888
Nicolas Cabrera <nicolascc1@hotmail.com>
2020-01-03 11:05:25 +0000

    added all project images

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -2,10 +2,15 @@ import React from 'react';
 import './Projects.css';
 import kodflix from '../images/kodflix.JPG';
 import footapp from '../images/footapp.JPG';
+import ludo from '../images/ludo.jpg';
+import weatherapp from '../images/weatherapp.jpg';
+import calculator from '../images/calculator.jpg';
+import bookingSystem from '../images/bookingsystem.jpg';
+
 
 const Projects = () => {
     return (
-        <>
+        <div>
             <div className='section' id='projects'>
                 <h1 className='title'>Projects</h1>
                 <div className='project-sec'>
@@ -18,22 +23,26 @@ const Projects = () => {
                         <img src={footapp} alt='Footapp' />
                     </div>
                     <div className='proj-item'>
-                        <h2>Calculator</h2>
+						<h2>Calculator</h2>
+						<img src={calculator} alt='Calculator' />
                     </div>
                 </div>
                 <div className='project-sec'>
                     <div className='proj-item'>
-                        <h2>Weather App</h2>
+						<h2>Weather App</h2>
+						<img src={weatherapp} alt='WeatherApp' />
                     </div>
                     <div className='proj-item'>
-                        <h2>Booking System</h2>
+						<h2>Booking System</h2>
+						<img src={bookingSystem} alt='BookingSystem' />
                     </div>
                     <div className='proj-item'>
-                        <h2>Ludo Game</h2>
+						<h2>Ludo Game</h2>
+						<img src={ludo} alt='Ludo' />
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
